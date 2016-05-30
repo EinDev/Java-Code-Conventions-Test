0b326b5aba6cb6d35c14d08e4134eb33e0576b6b
TimonMi <timon.michel@deko-rolf.de>
2016-05-30 20:02:48 +0200

    Basically made some nice formatting, added a few comments and hopefully in the right way. Should be perfect formatted.

--- src/info/mymc/codeconventiontest/Main.java
+++ src/info/mymc/codeconventiontest/Main.java
@@ -1,24 +1,55 @@
+/**
+ * Main.java
+ *
+ * Just a basic project to test the functionality of IntelliJ IDEA and
+ * practising the Java Code Conventions. I was getting notified by a nice
+ * guy to check out the Code Conventions, so i decided to take a look at it.
+ *
+ * By TimonMi, 2016
+ * No Copyright
+ */
 package info.mymc.codeconventiontest;
 
 /**
+ * The Main class provides anything needed in this project.
+ * It basicially contains just random functions, no actual magic.
  *
- * Just a basic project to test the functionality of IntelliJ IDEA and practising the Java Code Conventions.
- *
- * I was getting notified by a nice guy to check out the Code Conventions, so i decided to take a look at it.
- *
+ * @version 1.0.0
+ * @author TimonMi
  */
-
 public class Main {
 
+    /** Just a random integer */
+    public static int one = 0;
+    /** Yet another random integer */
+    public static int three = 3;
+    /* Six. Just random */
+    private int six = 6;
+
+
+    public Main() {
+
+    }
+
+
     /**
-     *
      * Main method
      *
      * @param args The arguments
      */
-
     public static void main(String[] args) {
 
     }
+
+    /**
+     * Just gives out a number
+     *
+     * @return The number '3'
+     */
+    public static int giveMeThree() {
+        return three;
+    }
+
+
 
 }
