c0c3c8a93d815ad878fe86eef1b8884785d4e5b8
TimonMi <timon.michel@deko-rolf.de>
2016-05-30 16:46:05 +0200

    Added some nice comments

--- src/info/mymc/codeconventiontest/Main.java
+++ src/info/mymc/codeconventiontest/Main.java
@@ -1,10 +1,24 @@
 package info.mymc.codeconventiontest;
 
 /**
- * Created by timon on 30.05.16.
+ *
+ * Just a basic project to test the functionality of IntelliJ IDEA and practising the Java Code Conventions.
+ *
+ * I was getting notified by a nice guy to check out the Code Conventions, so i decided to take a look at it.
+ *
  */
+
 public class Main {
 
+    /**
+     *
+     * Main method
+     *
+     * @param args The arguments
+     */
 
+    public static void main(String[] args) {
+
+    }
 
 }
